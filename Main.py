eace847995e1a4de4446cf781b77e9ace197a064
MrAndrewlol <40283828+MrAndrewlol@users.noreply.github.com>
2023-08-24 18:59:38 -0600

    Feat:Creacion de tk inter y turtle

--- Main.py
+++ Main.py
@@ -1,1 +1,48 @@
-print("Campo Eléctrico para diferentes figuras")
+# import turtle
+
+
+# tortugaxy = turtle.Turtle()
+
+
+# print("Campo Eléctrico para diferentes figuras")
+
+
+# tortugaxy.setup(600,600)
+# tortugaxy.penup()
+# tortugaxy.goto(-200,-100)
+# tortugaxy.pendown()
+# tortugaxy.forward(400)
+# tortugaxy.penup()
+# tortugaxy.goto(-200,-200)
+# tortugaxy.pendown()
+# tortugaxy.left(90)
+# tortugaxy.forward(400)
+
+
+# k = input("Desea salir?")
+
+import turtle
+import tkinter as tk
+
+
+def do_stuff():
+    for color in ["red", "yellow", "green"]:
+        my_lovely_turtle.color(color)
+        my_lovely_turtle.right(120)
+
+
+def press():
+    do_stuff()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    canvas = tk.Canvas(root)
+    canvas.config(width=600, height=200)
+    canvas.pack(side=tk.LEFT)
+    screen = turtle.TurtleScreen(canvas)
+    screen.bgcolor("cyan")
+    button = tk.Button(root, text="Press me", command=press)
+    button.pack()
+    my_lovely_turtle = turtle.RawTurtle(screen, shape="turtle")
+    root.mainloop()
